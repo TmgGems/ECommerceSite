42817bd445f9a4d8ab2cb769c49292ae84774405
Gyalwang Ghhyamjo lama <gyalwanglama123@gmail.com>
2024-05-20 16:52:53 +0545

    Repositroy Pattern Implemented

---
+++ ECommereceSiteData/Repository/CategoryRepository.cs
@@ -0,0 +1,36 @@
+﻿using ECommereceSiteData.Data;
+using ECommereceSiteData.Repository.IRepository;
+using ECommereceSiteModels.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommereceSiteData.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepositroy
+    {
+        private ApplicationDbContext _db;
+        public CategoryRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        public void Update(Category obj)
+        {
+            var objFromDb = _db.Categories.FirstOrDefault(u => u.Id == obj.Id);
+
+            if (objFromDb != null)
+            {
+                objFromDb.CategoryName = obj.CategoryName;
+
+                if(objFromDb.ImageUrl != null)
+                {
+                    objFromDb.ImageUrl = obj.ImageUrl;
+
+                }
+
+            }
+        }
+    }
+}
---
+++ ECommereceSiteData/Repository/IRepository/IRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommereceSiteData.Repository.IRepository
+{
+    public interface IRepository <T> where T : class
+    {
+        //T - category or any other  model that we want to perform CRUD operations
+        IEnumerable<T> GetAll();
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+
+
+        void Remove(T entity);
+
+        void RemoveRange(IEnumerable<T> entity);
+    }
+}
