3f45866e9620e5cd7eeac1664c1fb65981f55ffd
Gyalwang Ghhyamjo lama <gyalwanglama123@gmail.com>
2024-05-28 13:52:33 +0545

    Details and Home Page Done

---
+++ ECommereceSiteModels/Models/ShoppingCart.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommereceSiteModels.Models
+{
+    public class ShoppingCart
+    {
+        //public int Id {  get; set; }
+
+        //public int ProductId {  get; set; }
+        //[ValidateNever]
+        //[ForeignKey("ProductId")]
+
+        //public Product Product { get; set; }
+
+        //public int Count {  get; set; }
+
+        //public int ApplicationUserId {  get; set; }
+        //[ForeignKey("ApplicationUser")]
+        //[ValidateNever]
+        //public AppplicationUser ApplicationUser { get; set; }
+    }
+}
